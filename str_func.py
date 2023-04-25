6be968aa2f310e118358920acceb366895027507
AnnaKolmychek <a.kolmychek@gmail.com>
2023-04-25 22:20:50 +0300

    add func str_title with docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,6 @@
 def str_upper(value: str) -> str:
     return value.upper()
+
+def str_title(value: str) -> str:
+    """make first letters uppercase / docstring from jun"""
+    return value.title()
